ba9f9a9c2e275a55c9fc2a1fb52582b8ad54e40b
Aymane D. Hlamnach <dara-@outlook.jp>
2024-11-19 22:08:36 +0100

    added guar_org

---
+++ guar_org.sh
@@ -0,0 +1,37 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+
+function create {
+    name=$1
+    org=$2
+    token=$3
+
+    docker rm -f $name >/dev/null 2>&1 || true
+
+    docker run -d --restart=always \
+        --runtime=sysbox-runc \
+        -e ORG_NAME="$org" \
+        -e RUNNER_SCOPE="org" \
+        -e RUNNER_TOKEN="$token" \
+        -e RUNNER_NAME="$name" \
+        -e RUNNER_GROUP="" \
+        -e LABELS="" \
+        --name "$name" azuradara/guar:latest 
+
+    sleep 4
+
+    docker exec -w /actions-runner "$name" "../main.sh"
+}
+
+function main() {
+    if [[ $# -ne 4 ]]; then
+        printf "\nerror: Unexpected number of arguments provided\n"
+        printf "\nUsage: ./guar.sh <runner-name> <org> <runner-token>\n\n"
+        exit 2
+    fi
+
+    create "$1" "$2" "$3"
+}
+
+main "$@"
