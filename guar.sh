932a01a540fab67df8618bda4495fc38025945e5
Aymane D. Hlamnach <dara-@outlook.jp>
2024-11-19 22:03:21 +0100

    added an org runner script

--- guar.sh
+++ guar.sh
@@ -5,14 +5,14 @@ set -o nounset
 function create {
     name=$1
     org=$2
-    repo=$3
-    token=$4
+    token=$3
 
     docker rm -f $name >/dev/null 2>&1 || true
 
     docker run -d --restart=always \
         --runtime=sysbox-runc \
-        -e REPO_URL="https://github.com/${org}/${repo}" \
+        -e ORG_NAME="$org" \
+        -e RUNNER_SCOPE="org" \
         -e RUNNER_TOKEN="$token" \
         -e RUNNER_NAME="$name" \
         -e RUNNER_GROUP="" \
@@ -27,11 +27,11 @@ function create {
 function main() {
     if [[ $# -ne 4 ]]; then
         printf "\nerror: Unexpected number of arguments provided\n"
-        printf "\nUsage: ./guar.sh <runner-name> <org> <repo-name> <runner-token>\n\n"
+        printf "\nUsage: ./guar.sh <runner-name> <org> <runner-token>\n\n"
         exit 2
     fi
 
-    create "$1" "$2" "$3" "$4"
+    create "$1" "$2" "$3"
 }
 
 main "$@"
